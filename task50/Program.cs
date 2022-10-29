c8106ae24855ac644d21384d931495f80bdafed8
Viktoria <vikystrelnikova@yandex.ru>
2022-10-29 11:42:53 +0500

    dvumerny massiv

--- task50/Program.cs
+++ task50/Program.cs
@@ -1,33 +1,41 @@
 ﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
+
 void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-        {
             matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
             Console.Write($"{matrix[i, j]} \t");
-        }
     Console.WriteLine();
     }
 }
+
 Console.Clear();
-int[,] matrix = new int[4, 4];
-InputMatrix(matrix);
-Console.WriteLine("Введите № строки: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите № столбца: ");
+Console.Write("Введите кол-во строк: ");
 int n = Convert.ToInt32(Console.ReadLine());
-void FindIndexMatrix(int[,] matrix)
-{
-    if (m >= 0 && m < matrix.GetLength(0) && n >= 0 && n < matrix.GetLength(1))
-    {
-        Console.WriteLine(matrix[m-1, n-1]);
-    }
-    else
-    {
-        Console.WriteLine("Такого числа в массиве нет");
-    }
-}
-FindIndexMatrix(matrix);
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+Console.Write("Введите строку нужного элемента: ");
+int nn = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец нужного элемента: ");
+int mm = Convert.ToInt32(Console.ReadLine());
+
+if ( nn > n || mm > m) 
+    Console.WriteLine("Такого элемента нет");
+
+else Console.Write($"Ваш элемент: {matrix[nn - 1, mm - 1]} \t");
